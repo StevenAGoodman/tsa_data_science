51c8701af6187257ed3d29f6edd9053967d90217
Steven A. Goodman <163490937+StevenAGoodman@users.noreply.github.com>
2024-09-11 17:25:14 -0400

    Create main.py

---
+++ impromptu/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def main(data_loc):
+  df = pd.read_csv(data_loc)
+
+df['column'].min()
+df['column'].max()
+df['column'].mean()
+
+scipy
+import seaborn as sns
+from matplotlib import pyplot
+
