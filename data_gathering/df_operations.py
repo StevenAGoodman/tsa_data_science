be676e97d78a8167d2571bb100c5b83e5e3dcba1
Steven A. Goodman <163490937+StevenAGoodman@users.noreply.github.com>
2024-10-30 15:07:36 -0400

    update functions

--- data_gathering/df_operations.py
+++ data_gathering/df_operations.py
@@ -4,7 +4,7 @@ import linecache
 column_names = ["id", "format", "title", "old_title", "#", 'release_year', '#2', 'runtime', 'genre']
 df = pd.DataFrame()
 
-def type_filtering():
+def imdb_og_filtering():
     with open("./data_gathering/imdb_processed_titles.tsv", 'r') as title_file:
         for line in title_file:
             line = line.split('\t')
@@ -13,14 +13,22 @@ def type_filtering():
                 data = pd.DataFrame(data, index=0)
                 df = pd.concat([df, data], axis=0)
 
-def main():
+def imdb_titles_rating_merg():
     title_df = pd.read_csv("./data_gathering/imdb_processed_titles.tsv", sep="\t")
     df.set_index(0, inplace=True)
     ratings_df = pd.read_csv("./data_gathering/title.ratings.tsv", sep="\t")
     df.set_index('tconst', inplace=True)
             
     df = pd.merge(title_df, ratings_df, left_index=True, right_index=True)
-    df.set_index(0)
-    df.rename(columns={1:"title", 2:"avg_imdb_rating", 3:"runtime_min"})
-    df.to_csv("./data_gathering/imdb_ratings.csv")
+    df.rename(columns={0:"id", 1:"title", 2:"avg_imdb_rating", 3:"runtime_min"})
+    df.index("title")
+    return df
 
+def main():
+    imdb_df = imdb_titles_rating_merg()
+    numbers_df = pd.read_csv("./data_gathering/theNumbers_finance.csv")
+    numbers_df.index("title")
+    
+    df = pd.concat([imdb_df, numbers_df], axis=1)
+    df.to_csv("./data_gathering/imdb_ratings.csv")
+    
